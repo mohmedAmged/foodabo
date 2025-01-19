74a7a3c812340eca94724389d81967d2a1729fb1
Mohamed Amged <mohamedamged55555@gmail.com>
2025-01-19 19:51:16 +0200

    make update in image in single category page

--- src/components/SectionHeroArchivePage/SectionHeroArchivePage.tsx
+++ src/components/SectionHeroArchivePage/SectionHeroArchivePage.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactNode } from "react";
-import imagePng from "images/hero-right2.png";
+import imagePng from "../../images/hero-right2.png";
+
 import HeroSearchForm, {
   SearchTab,
 } from "components/HeroSearchForm/HeroSearchForm";
